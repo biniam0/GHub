9f406424e7da29327d365af9634591dc71c6903a
bini <jaletakebede@gmail.com>
2025-05-24 14:39:02 +0300

    Expand feature for the game description using Expandable component

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,21 +1,22 @@
-import { useParams } from 'react-router-dom'
-import useGame from '../hooks/useGame'
-import { Box, Heading, Spinner, Text } from '@chakra-ui/react'
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+import { Box, Button, Heading, Spinner, Text } from "@chakra-ui/react";
+import ExpandableText from "../components/ExpandableText";
 
 const GameDetailPage = () => {
-  const {slug} = useParams()
-  const {data: game, error, isLoading, } = useGame(slug!)
+  const { slug } = useParams();
+  const { data: game, error, isLoading } = useGame(slug!);
 
-  if (isLoading) return <Spinner />
+  if (isLoading) return <Spinner />;
 
-  if (error || !game) throw error
+  if (error || !game) throw error;
 
   return (
     <Box>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </Box>
-  )
-}
+  );
+};
 
-export default GameDetailPage
+export default GameDetailPage;
